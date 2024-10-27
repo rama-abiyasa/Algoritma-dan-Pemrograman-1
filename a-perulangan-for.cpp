6c33361e83f3f0012a07ac00c30e420e7e61049d
rama-abiyasa <ramaabieyasa@gmail.com>
2024-10-27 21:43:46 +0700

    Update a-perulangan-for.cpp

--- a-perulangan-for.cpp
+++ a-perulangan-for.cpp
@@ -2,13 +2,14 @@
 
 int main() {
     printf("Bilangan\n\n");
+    //menampilkan bilangan ganjil
     printf("Ganjil: ");
     for (int i = 1; i <= 35; i++) {
         if (i % 2 != 0) {
             printf("%d ", i);
         }
     }
-    
+    //menampilkan bilangan genap
     printf("\nGenap: ");
     for (int i = 1; i <= 35; i++) {
         if (i % 2 == 0) {
